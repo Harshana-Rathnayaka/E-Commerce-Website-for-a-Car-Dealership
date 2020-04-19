3afbb127d3da15739264b32295b4376817382206
Harshana-Rathnayaka <54583124+Harshana-Rathnayaka@users.noreply.github.com>
2020-04-20 00:56:32 +0530

    Initial Commit

--- includes/dbConnection.php
+++ includes/dbConnection.php
@@ -14,12 +14,13 @@ class DbConnect
 		try {
 
 			// main server
-			$this->con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+			$this->con = new mysqli(BACKUP_DB_HOST, BACKUP_DB_USER, BACKUP_DB_PASSWORD, BACKUP_DB_NAME);
 
 			if (mysqli_connect_errno()) {
 
 				// backup server
-				$this->con = new mysqli(BACKUP_DB_HOST, BACKUP_DB_USER, BACKUP_DB_PASSWORD, BACKUP_DB_NAME);
+				$this->con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
 
 				if (mysqli_connect_errno()) {
 					echo "Failed to connect with the database" . mysqli_error($con);
