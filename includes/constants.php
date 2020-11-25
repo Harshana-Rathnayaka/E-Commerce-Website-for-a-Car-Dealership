da27722c6838d1d1815f4f31d85c1e65a4774f5a
Harshana ratnayaka <huratnayaka@gmail.com>
2020-11-26 02:47:24 +0530

    connection updated to work with one server

--- includes/constants.php
+++ includes/constants.php
@@ -5,9 +5,3 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 
-
-// backup server
-define('BACKUP_DB_HOST', 'dbinstance1.ccqoxdbiqxft.ap-south-1.rds.amazonaws.com');
-define('BACKUP_DB_USER', 'harsha');
-define('BACKUP_DB_PASSWORD', '0773341555');
-define('BACKUP_DB_NAME', 'ds-web');
--- includes/dbConnection.php
+++ includes/dbConnection.php
@@ -3,32 +3,18 @@
 class DbConnect
 {
 
-	private $con;
-	function __construct()
-	{ }
+    private $con;
+    public function __construct()
+    {}
 
-	function connect()
-	{
-		include_once dirname(__FILE__) . '/constants.php';
+    public function connect()
+    {
+        include_once dirname(__FILE__) . '/constants.php';
+        $this->con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-		try {
-
-			// main server
-			$this->con = new mysqli(BACKUP_DB_HOST, BACKUP_DB_USER, BACKUP_DB_PASSWORD, BACKUP_DB_NAME);
-
-			if (mysqli_connect_errno()) {
-
-				// backup server
-				$this->con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-				if (mysqli_connect_errno()) {
-					echo "Failed to connect with the database" . mysqli_error($con);
-				}
-				return $this->con;
-			}
-			return $this->con;
-		} catch (Exception $e) {
-			echo "Connection Failed" . $e->getMessage();
-		}
-	}
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect with the database" . mysqli_connect_error();
+        }
+        return $this->con;
+    }
 }
